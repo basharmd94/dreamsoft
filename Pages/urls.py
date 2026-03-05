83c9f7eb39ab9f84cab43e15342afb499e7a5065
basharmd94 <mat197150@gmail.com>
2026-03-05 13:51:00 +0600

    resturant pos software page implemented

--- Pages/urls.py
+++ Pages/urls.py
@@ -4,7 +4,31 @@ from .views import main_page_view
 urlpatterns = [
     path('', main_page_view.home, name='home'),
     path('fashion-shop-software/', main_page_view.fashion_shop_software, name='fashion_shop_software'),
+    # resturant post software
+    path('resturant-pos-software/', main_page_view.resturant_pos_software, name='resturant_pos_software'),
+    # supershop post software
+    path('supershop-pos-software/', main_page_view.supershop_pos_software, name='supershop_pos_software'),
+    # electronics shop post software
+    path('electronics-shop-pos-software/', main_page_view.electronics_shop_pos_software, name='electronics_shop_pos_software'),
+    # wholesale shop post software
+    path('wholesale-shop-pos-software/', main_page_view.wholesale_shop_pos_software, name='wholesale_shop_pos_software'),
+    # hardware shop post software
+    path('hardware-shop-pos-software/', main_page_view.hardware_shop_pos_software, name='hardware_shop_pos_software'),
+    # hotel management post software
+    path('hotel-management-software/', main_page_view.hotel_management_software, name='hotel_management_software'),
+    # tailor post software
+    path('tailor-software/', main_page_view.tailor_software, name='tailor_software'),
+    # accounting post software
+    path('accounting-software/', main_page_view.accounting_software, name='accounting_software'),
+    # inventory post software
+    path('inventory-software/', main_page_view.inventory_software, name='inventory_software'),
+    # pos post software
+    path('pos-software/', main_page_view.pos_software, name='pos_software'),
     path('erp-software/', main_page_view.erp_software, name='erp_software'),
+
+
+
+
     path('about-us/', main_page_view.about_us, name='about_us'),
     path('contact-us/', main_page_view.contact_us, name='contact_us'),
 ]
--- Pages/views/main_page_view.py
+++ Pages/views/main_page_view.py
@@ -8,6 +8,37 @@ def home(request):
 def fashion_shop_software(request):
     return render(request, 'Pages/fashion-shop-software.html')
 
+
+def resturant_pos_software(request):
+    return render(request, 'Pages/resturant-pos-software.html')
+
+def supershop_pos_software(request):
+    return render(request, 'Pages/supershop-pos-software.html')
+
+def electronics_shop_pos_software(request):
+    return render(request, 'Pages/electronics-shop-pos-software.html')
+
+def wholesale_shop_pos_software(request):
+    return render(request, 'Pages/wholesale-shop-pos-software.html')
+
+def hardware_shop_pos_software(request):
+    return render(request, 'Pages/hardware-shop-pos-software.html')
+
+def hotel_management_software(request):
+    return render(request, 'Pages/hotel-management-software.html')
+
+def tailor_software(request):
+    return render(request, 'Pages/tailor-shop-software.html')
+
+def accounting_software(request):
+    return render(request, 'Pages/accounting-software.html')
+
+def inventory_software(request):
+    return render(request, 'Pages/inventory-software.html')
+
+def pos_software(request):
+    return render(request, 'Pages/pos-software.html')
+
 def erp_software(request):
     return render(request, 'Pages/erp-software.html')
 
