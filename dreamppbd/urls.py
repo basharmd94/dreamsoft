c5b35ae9e6d18f195ce36c0275c05c2816a6a5e0
basharmd94 <mat197150@gmail.com>
2026-03-09 10:12:01 +0600

    admin url changed

--- dreamppbd/urls.py
+++ dreamppbd/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secure-panel/', admin.site.urls),
     path('', include('Pages.urls')),
 ]
 
